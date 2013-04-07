2363eb3a09d62e653541817a86b66a02833a3219
Gaspar Rajoy <gaspar@gaspareta.(none)>
2013-04-07 15:55:27 -0300

    Fixes

--- smarty/templates_c/%%A7^A7E^A7ED66A1%%paginador.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.19, created on 2013-04-07 15:40:06
-         compiled from paginador.tpl */ ?>
-<!--muestra la barra que contiene los links del paginador-->
-<?php if ($this->_tpl_vars['barra']): ?>
-	<div id="paginador">
-  		<?php echo $this->_tpl_vars['barra']; ?>
-
-	</div><!--paginador-->
-<?php endif; ?>
